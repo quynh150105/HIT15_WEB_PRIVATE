28d900697fff3f9ce4f04f42829dc6fc5bf36f4e
quynh150105 <quynh1512005@gmail.com>
2025-01-12 23:12:17 +0700

    add btvn

---
+++ BTVN_WEEK11/Bai4/main.js
@@ -0,0 +1,12 @@
+/*
+  document.addEventListener("keydown", function (event) {
+        console.log("Phím được nhấn: ", event.key); // Kiểm tra xem phím được nhấn có phải là phím Enter không
+        if (event.key === "Enter") {
+          console.log("Phím Enter đã được nhấn"); // Thực hiện hành động mong muốn ở đây
+        }
+      });
+*/
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const number_true = document.querySelector("#true");
+const number_false = document.querySelector("#false");
