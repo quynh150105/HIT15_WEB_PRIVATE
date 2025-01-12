6f256809178a0b1fed9539d683f33102a0c8fc4a
quynh150105 <quynh1512005@gmail.com>
2025-01-12 23:05:11 +0700

    add btvn week13

--- BTVN_WEEK13/Bai2/main.js
+++ BTVN_WEEK13/Bai2/main.js
@@ -1,19 +1,19 @@
-document.addEventListener("keydown", function (event) {
-  if (event.key === "F12") {
-    event.preventDefault();
-    alert("f12 bị vô hiệu hóa");
-  }
-});
-document.addEventListener("keydown", function (event) {
-  if (event.ctrlKey && event.key === "u") {
-    event.preventDefault();
-    alert("ctrl + u bị vô hiệu hóa");
-  }
-});
-document.addEventListener("contextmenu", function (event) {
-  event.preventDefault();
-  alert("chuột phải bị vô hiệu hóa");
-});
+// document.addEventListener("keydown", function (event) {
+//   if (event.key === "F12") {
+//     event.preventDefault();
+//     alert("f12 bị vô hiệu hóa");
+//   }
+// });
+// document.addEventListener("keydown", function (event) {
+//   if (event.ctrlKey && event.key === "u") {
+//     event.preventDefault();
+//     alert("ctrl + u bị vô hiệu hóa");
+//   }
+// });
+// document.addEventListener("contextmenu", function (event) {
+//   event.preventDefault();
+//   alert("chuột phải bị vô hiệu hóa");
+// });
 
 const input = document.querySelector(".input");
 const AddButton = document.querySelector(".Add");
